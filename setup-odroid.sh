742f3ae94bd892f9ba331830b3468c657d5d1375
Ben Oxley <ben.oxley@gmail.com>
2021-10-18 14:34:07 +0100

    Update setup-odroid.sh

--- setup-odroid.sh
+++ setup-odroid.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install docker
-sudo apt install curl
+sudo apt install docker -y
+sudo apt install curl -y
 sudo chmod +x /usr/local/bin/docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-linux-armv7" -o /usr/local/bin/docker-compose
+sudo apt install git -y
