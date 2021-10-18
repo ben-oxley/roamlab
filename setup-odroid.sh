35d911fa7ed80e6d7d8acd50786d9cf6839effd8
Ben Oxley <ben.oxley@gmail.com>
2021-10-18 14:52:22 +0100

    Update setup-odroid.sh

--- setup-odroid.sh
+++ setup-odroid.sh
@@ -1,8 +1,21 @@
+#To grab this scrip, you'll need git
+#sudo apt install git -y
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install docker -y
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo apt install curl -y
 sudo chmod +x /usr/local/bin/docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-linux-armv7" -o /usr/local/bin/docker-compose
-sudo apt install git -y
+
