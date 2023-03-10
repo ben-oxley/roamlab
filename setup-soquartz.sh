71e908068754339c6f469b6e7522bf20dcc8dddd
Ben Oxley <ben.oxley@gmail.com>
2023-03-10 11:25:51 +0000

    Update setup-soquartz.sh

--- setup-soquartz.sh
+++ setup-soquartz.sh
@@ -9,8 +9,6 @@ sudo apt-get -y install podman
 #Install ceph following guide here: https://docs.ceph.com/en/quincy/cephadm/install/#cephadm-deploying-new-cluster
 curl --silent --remote-name --location https://github.com/ceph/ceph/raw/quincy/src/cephadm/cephadm
 chmod +x cephadm
-./cephadm add-repo --release quincy
-sudo apt update
 sudo apt install -y cephadm
 ipaddr=$(ip a s eth0 | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2)
 cephadm bootstrap --mon-ip $ipaddr
