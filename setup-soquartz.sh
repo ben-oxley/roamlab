e588aa73e32c498ee257da9219bc809f38278d50
Ben Oxley <ben.oxley@gmail.com>
2023-03-27 21:43:41 +0100

    Update setup-soquartz.sh

--- setup-soquartz.sh
+++ setup-soquartz.sh
@@ -1,7 +1,19 @@
+#!/usr/bin/env bash
 sudo apt update
 sudo mkfs.ext4 -E nodiscard /dev/nvme0n1
 mkdir /data
 sudo mount -o discard /dev/nvme0n1 /data
+
+#Install influx
+# influxdata-archive_compat.key GPG fingerprint:
+#     9D53 9D90 D332 8DC7 D6C8 D3B9 D8FF 8E1F 7DF8 B07E
+wget -q https://repos.influxdata.com/influxdata-archive_compat.key
+echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
+sudo apt-get update && sudo apt-get install -y influxdb2
+sudo service influxdb start
+sudo apt install -y telegraf
+# configure influx and telegraf config at /etc/telegraf/telegraf.conf
 
 #Install podman
 sudo apt-get -y install podman
